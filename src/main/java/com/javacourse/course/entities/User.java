76c3df3b8ff341b9d28038026f90776497ea8214
Rafael Schiavon <raafaschi@gmail.com>
2021-07-15 22:45:07 -0300

    User insert

--- src/main/java/com/javacourse/course/entities/User.java
+++ src/main/java/com/javacourse/course/entities/User.java
@@ -23,7 +23,7 @@ public class User implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	private String nome;
+	private String name;
 	private String email;
 	private String phone;
 	private String password;
@@ -35,10 +35,10 @@ public class User implements Serializable {
 	public User() {
 	}
 
-	public User(Long id, String nome, String email, String phone, String password) {
+	public User(Long id, String name, String email, String phone, String password) {
 		super();
 		this.id = id;
-		this.nome = nome;
+		this.name = name;
 		this.email = email;
 		this.phone = phone;
 		this.password = password;
@@ -53,11 +53,11 @@ public class User implements Serializable {
 	}
 
 	public String getNome() {
-		return nome;
+		return name;
 	}
 
-	public void setNome(String nome) {
-		this.nome = nome;
+	public void setNome(String name) {
+		this.name = name;
 	}
 
 	public String getEmail() {
